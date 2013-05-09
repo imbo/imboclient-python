dc8290cb1bd6b1a254c1254952878c80755cd572
Andreas Søvik <arsovik@gmail.com>
2013-05-09 20:22:10 +0200

    Fixed PiPy setup. More initial directory structure.

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-m distutils.core import setup
+# coding=utf-8
+from setuptools import setup
 
 setup(
         name='imbo',
         version='0.0.1',
         author='Andreas Søvik',
         author_email='arsovik@gmail.com',
-        packages=['imbo'],
+        packages=['imboclient'],
         scripts=[],
         url='http://www.imbo-project.org',
         license='LICENSE.txt',
         description='Python client for Imbo',
-        long_description=open('README.markdown').read(),
-        install_requires=[],
+        long_description=open('README.md').read(),
+        install_requires=['requests', 'nose'],
 )
 
