021238316d31ac402ffa5f48ec8b48c5a65ef159
Andreas Søvik <arsovik@gmail.com>
2013-11-22 21:31:54 +0100

    Added plaaceholder tests for image transformations

--- imboclient/test/url/test_image.py
+++ imboclient/test/url/test_image.py
@@ -1,3 +1,5 @@
+import imboclient
+from mock import patch
 from imboclient.url import image
 
 class TestUrlImage:
@@ -15,4 +17,58 @@ class TestUrlImage:
     def test_resource_url(self):
         test_result = self._url_image.resource_url()
         assert test_result == 'http://imbo.local/users/public/ffffffffffffffffffffffffffffffff'
+
+    def test_border_default(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_border(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_compress(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_convert(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_gif(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_jpg(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_png(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_crop(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_flip_horizontally(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_flip_vertically(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_resize(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_max_size(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_rotate(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_thumbnail(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_canvas(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_desaturate(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_sepia(self):
+        raise NotImplementedError("Test and implementation missing")
+
+    def test_reset(self):
+        raise NotImplementedError("Test and implementation missing")
 
--- imboclient/url/image.py
+++ imboclient/url/image.py
@@ -10,3 +10,53 @@ class UrlImage (url.Url):
     def resource_url(self):
         return self._base_url + '/users/' + self._public_key + '/' + self._image_identifier
 
+    def border(self, color = '000000', width = 1, height = 1):
+        return
+
+    def compress(self, quality = 75):
+        return
+
+    def convert(self, ctype):
+        return
+
+    def gif(self):
+        return
+
+    def jpg(self):
+        return
+
+    def png(self):
+        return
+
+    def crop(self, x, y, width, height):
+        return
+
+    def flip_horizontally(self):
+        return
+
+    def flip_vertically(self):
+        return
+
+    def resize(self, width, height):
+        return
+
+    def max_size(self, max_width, max_height):
+        return
+
+    def rotate(self, angle, bg = '000000'):
+        return
+
+    def thumbnail(self, width = 50, height = 50, fit = 'outbound'):
+        return
+
+    def canvas(self, width, height, mode = None, x = None, y = None, bg = None):
+        return
+
+    def transponse(self):
+        return
+
+    def transverse(self):
+        return
+
+    def desaturate(self):
+        return
