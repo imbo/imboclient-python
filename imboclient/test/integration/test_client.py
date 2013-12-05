0439c798269586caea61c8aff045c479a296f688
Andreas Søvik <arsovik@gmail.com>
2013-12-05 17:27:23 +0100

    Start integration testing

---
+++ imboclient/test/integration/test_client.py
@@ -0,0 +1,12 @@
+import imboclient.test.integration.config as config
+
+class TestClient:
+    def setup(self):
+        return
+
+    def teardown(self):
+        return
+
+    def test_add_image(self):
+        print config.server['host']
+        return
