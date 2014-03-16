67567c8943607601ac3deb0fd56aa3585a7e3c69
Andreas Søvik <arsovik@gmail.com>
2014-03-16 18:01:10 +0100

    Include default config used by travis for integration tests

---
+++ imboclient/test/integration/config.py
@@ -0,0 +1,8 @@
+# This configuration is used by the integration tests. They WILL modify the imbo datasources. Be cautious.
+server = {
+        "host": "127.0.0.1",
+        "port": "9012",
+        "public": "test",
+        "private": "test"
+        }
+
