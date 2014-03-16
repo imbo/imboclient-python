5d3ff3fc9fa69f292ec7ba71dd3ae1e01fb55050
Andreas Søvik <arsovik@gmail.com>
2014-03-16 18:09:02 +0100

    Imbolauncher path

--- imboclient/test/travis.sh
+++ imboclient/test/travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 php composer.phar create-project --prefer-source --no-dev --no-interaction imbo/imbolauncher imbolauncher dev-develop
-./integration/imbolauncher/imbolauncher/imbolauncher start-servers --config=$TRAVIS_BUILD_DIR/imboclient/test/integration/imbolauncher/config.json --install-path=/tmp/imbo-servers --no-interaction -vvv ; cat /tmp/imbo-servers/dev-develop/httpd.log
+./integration/imbolauncher/imbolauncher start-servers --config=$TRAVIS_BUILD_DIR/imboclient/test/integration/imbolauncher/config.json --install-path=/tmp/imbo-servers --no-interaction -vvv ; cat /tmp/imbo-servers/dev-develop/httpd.log
 nosetests
 OUT=$?
 
@@ -13,7 +13,7 @@ else
     curl http://127.0.0.1:9012/users/test
 fi
 
-./integration/imbolauncher/imbolauncher/imbolauncher kill-servers --no-interaction -vvv
+./integration/imbolauncher/imbolauncher kill-servers --no-interaction -vvv
 
 exit $OUT
 
